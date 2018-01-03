6d614920d9053dc7094af380050a4cb4ab600106
Lambert Lum <emceelam@gmail.com>
2018-01-03 12:37:04 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# NAME
+
+Multi Chat C - A socket-based multi-client chat written with C code
+
+# SYNOPSIS
+
+Open terminal
+
+    perl server0.pl
+
+From other terminal
+
+    telnet 127.0.0.1 4023
+
+From yet another terminal
+
+    telnet 127.0.0.1 4023
+
+Start typing text in your telnet sessions. Typing "quit" will cause the connection to terminate. Typing nothing for 10 seconds will cause connection to terminate.
+
+# DESCRIPTION
+
+This is a socket-based multi-client chat that echoes one user's chat to other user's chat telnet sessions.
+
+# FEATURES
+
+* Multiple telnet chat sessions
+    * select()
+    * non-blocking read/write sockets
+
+# AUTHOR
+
+Lambert Lum
+
+![email address](http://sjsutech.com/small_email.png)
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2018 by Lambert Lum
+
+This is free software; you can redistribute it and/or modify it under the same terms as the Perl 5 programming language system itself.
---
+++ server0.pl
@@ -0,0 +1,315 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use Socket qw(:DEFAULT :crlf);
+use Fcntl qw(F_GETFL F_SETFL O_NONBLOCK);
+use POSIX;
+use autodie qw(:socket fcntl sysread syswrite);
+use Data::Dumper;
+
+
+
+my $PORT = 4023;
+my $TIME_OUT = 10;
+
+my $sigset    = POSIX::SigSet->new(SIGPIPE);
+my $sigaction = POSIX::SigAction->new(sub { exit(0) } , $sigset, SA_NOCLDSTOP);
+
+
+=begin comment
+
+  // set up listenfd socket
+  int listenfd;
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (listenfd < 0) {
+    perror ("socket() failed");
+    exit(1);
+  }
+
+=end comment
+=cut
+
+my $listen_handle;
+socket($listen_handle, AF_INET, SOCK_STREAM, 0);
+
+=begin comment
+  // only has meaning if children are still using same port as parent,
+  // and you restart the parent
+  int on = 1;
+  if (setsockopt (listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
+    perror("setsockopt() fails");
+    exit(1);
+  }
+=end comment
+=cut
+
+setsockopt($listen_handle, SOL_SOCKET, SO_REUSEADDR, pack("l", 1));
+
+=begin comment
+  # bind
+  struct sockaddr_in sin;
+  sin.sin_family = AF_INET;
+  sin.sin_port = htons(PORT);
+  sin.sin_addr.s_addr = htonl(INADDR_ANY);
+  if (bind(listenfd, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+    perror ("Binding error in server!");
+    exit(1);
+  }
+=end comment
+=cut
+
+my $sockaddr = pack_sockaddr_in ($PORT, INADDR_ANY);
+bind ($listen_handle, $sockaddr);
+
+=begin comment
+  // listen
+  int backlog = 5;   // max length of queue of pending connections.
+  if (listen(listenfd, backlog) < 0) {
+    perror ("Listen error in server!");
+    exit(1);
+  }
+  printf ("Listening on socket %d for client connections\n", listenfd);
+
+=end comment
+=cut
+
+my $backlog = 5;
+listen ($listen_handle, $backlog);
+
+=begin comment
+  // main loop
+  // block for incoming socket communications
+  fd_set readset;
+  char chat[CHAT_SIZE];
+  int connfd;
+  int return_val;
+  FD_ZERO(&saveset);
+  FD_SET(listenfd, &saveset);
+  memset (sock_expiry, 0, sizeof(sock_expiry));
+  while(1) {
+    memcpy (&readset, &saveset, sizeof(fd_set));
+    return_val =
+      pselect (
+        MAX_FD,      // nfds, number of file descriptors
+        &readset,    // readfds
+        NULL,        // writefds
+        NULL,        // exceptfds, almost never used
+        NULL,        // struct timeval *timeout, NULL for always block
+        &empty_set   // sigmask, allow signal processing during select
+      );
+    if (return_val == -1 && errno == EINTR) {
+      // signal handling has interrupted pselect()
+      continue;
+    }
+=end comment
+=cut
+
+my %fd_to_handle;
+my $listen_fd = fileno($listen_handle);
+$fd_to_handle{$listen_fd} = $listen_handle;
+
+
+my $rin = "";
+my $rout;
+vec($rin, $listen_fd, 1) = 1;
+my $loop_cnt = 0;
+my %fd_to_expiry;
+while (1) {
+  # print "rin: " . unpack ("H*", $rin) . "\n";
+  select ($rout = $rin, undef, undef, 10);
+
+=begin comment
+
+    // incoming connection
+    int now = time(NULL);
+    if (FD_ISSET(listenfd, &readset)) {
+      connfd = accept(listenfd, NULL, NULL);
+        // will not block because accept has data
+      printf ("Connection from socket %d\n", connfd);
+      FD_SET(connfd, &saveset);
+      sock_expiry[connfd] = now + TIME_OUT;
+=end comment
+=cut
+
+  #print "loop_cnt: " . $loop_cnt++ . "\n";
+  #print "rout hex:  " . unpack ("H*", $rout) . "\n";
+  #print "rout bits: " . unpack ("B*", $rout) . "\n";
+  my $max_fd = length unpack ("B*", $rout);
+  my $now = time();
+  if (vec($rout, $listen_fd, 1)) {
+    my $conn_handle;
+    my $paddr = accept ($conn_handle, $listen_handle);
+    my ($port, $iaddr) = sockaddr_in($paddr);
+    my $name = gethostbyaddr($iaddr, AF_INET);
+    my $conn_fd = fileno($conn_handle);
+    $fd_to_handle{$conn_fd} = $conn_handle;
+    $fd_to_expiry{$conn_fd} = $now + $TIME_OUT;
+    vec($rin, $conn_fd, 1) = 1;
+    print ("accepted conn $conn_fd from $name, $port\n");
+
+
+=begin comment
+      /*
+       * set no linger
+       * onoff=1 and ling=0:
+       *   The connection is aborted on close,
+       *   and all data queued for sending is discarded.
+       *                   
+       *  struct linger {
+       *       int l_linger;   // how many seconds to linger for
+       *       int l_onoff;    // linger active
+       *   };
+       */
+      struct linger stay;
+      stay.l_onoff  = 1;
+      stay.l_linger = 0;
+      setsockopt(connfd, SOL_SOCKET, SO_LINGER, &stay, sizeof(struct linger));
+=end comment
+=cut
+
+    my $packed = pack("l l", 0, 1);
+    #print "SO_LINGER packed: " . unpack ("H*", $packed) . "\n";
+    setsockopt($conn_handle, SOL_SOCKET, SO_LINGER, $packed);
+
+=begin comment
+      // set connfd to be non-blocking socket
+      int val;
+      val = fcntl(connfd, F_GETFL, 0);
+      if (val < 0) {
+        perror ("fcntl(F_GETFL) failed");
+      }
+      val = fcntl(connfd, F_SETFL, val | O_NONBLOCK);
+      if (val < 0) {
+        perror ("fcntl(F_SETFL) failed");
+      }
+    }
+=end comment
+=cut
+
+    my $flags = fcntl($conn_handle, F_GETFL, 0);
+    fcntl($conn_handle, F_SETFL, $flags | O_NONBLOCK);
+  }
+
+=begin comment
+    // existing connections
+    int readsock;
+    for (readsock = listenfd +1; readsock < MAX_FD ; readsock++) {
+      if (!FD_ISSET(readsock, &readset)) {
+        continue;
+      }
+      
+=end comment
+=cut
+  foreach my $read_fd ( ($listen_fd+1)..$max_fd) {
+    if (!vec ($rout, $read_fd, 1)) {
+      next;
+    }
+    print "socket fd $read_fd is readable\n";
+  
+
+=begin comment
+
+      memset (chat, 0, CHAT_SIZE);
+      int nread = read (readsock, chat, CHAT_SIZE);
+      if (nread < 0) {
+        fprintf (stderr,
+          "read() failed on sock %d: %s\n", readsock, strerror(errno));
+        exit(1);
+      }
+      chat[strcspn(chat, "\r\n")] = '\0';  // chomp
+      sock_expiry[readsock] = now + TIME_OUT;
+
+=end comment
+=cut
+    my $buf = pack ("C80",0);
+    my $handle = $fd_to_handle{$read_fd};
+    my $nread = sysread ($handle, $buf, 80);
+    #print "nread: $nread\n";
+    $fd_to_expiry{$read_fd} = $now + $TIME_OUT;
+    #my @chars = unpack ("A A A A A A A A A A A A A A A A A", $buf);
+    #print "chars: " . Dumper \@chars;
+    my $str = unpack ("A*", $buf);
+    print "read $read_fd: $str\n";
+
+=begin comment
+      if ( nread == 0  // disconnected socket
+        || strcmp(chat, "quit") == 0) // user quitting
+      {
+        FD_CLR (readsock, &saveset);
+        close(readsock);
+        sock_expiry[readsock] = 0;
+        printf ("Quit from socket %d\n", readsock);
+        continue;
+      }
+      if (strlen(chat) == 0) {
+        continue;  // nothing to do
+      }
+      printf ("Read socket %d: '%s'\n", readsock, chat);
+=end comment
+=cut
+    if ( $nread == 0   # disconnected socket
+      || $str eq "quit")   # user quitting
+    { 
+      delete $fd_to_handle{$read_fd};
+      delete $fd_to_expiry{$read_fd};
+      vec($rin, $read_fd, 1) = 0;
+      print "Quit from socket $read_fd\n";
+      close($handle);
+      next;
+    }
+    if (length($str) == 0) {
+      next; # nothing to do
+    }
+
+=begin comment
+      // write chat to all other clients
+      int writesock;
+      int writecnt = 0;
+      struct timeval tv;
+      fd_set writeset;
+      memcpy (&writeset, &saveset, sizeof(fd_set));
+      tv.tv_sec  = 0;
+      tv.tv_usec = 0;
+      select (MAX_FD, NULL, &writeset, NULL, &tv);
+        // which sockets can we write to
+      for (writesock = listenfd +1; writesock < MAX_FD; writesock++) {
+        if (  writesock == readsock   // don't echo to originator 
+          || !FD_ISSET(writesock, &writeset))
+        {
+          continue;
+        }
+
+        printf ("Write socket %d: %s\n", writesock, chat);
+        writecnt++;
+        if (write (writesock, chat, CHAT_SIZE) < 0) {
+          fprintf (stderr,
+            "write() failed on sock %d: %s\n", writesock, strerror(errno));
+          exit(1);
+        }
+      }
+      if (!writecnt) {
+        printf ("No other telnet sessions. Message not copied: '%s'\n", chat);
+      }
+
+    }//for loop for existing connections
+
+    start_end_alarm(sock_expiry, &saveset);
+
+  }//while(1)
+
+
+=end comment
+=cut
+    while (my ($write_fd, $handle) = each %fd_to_handle) {
+      if ( $write_fd == $listen_fd 
+        || $write_fd == $read_fd)
+      {
+        next;
+      }
+      syswrite ($handle, "$str\n");
+      print ("write $write_fd: $str\n");
+    }
+  }
+}
+
